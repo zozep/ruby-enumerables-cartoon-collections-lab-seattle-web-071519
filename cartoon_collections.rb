9af1e1305bdb433d4aac6e39f4ff2150081ba83a
Joseph P <jpark041@gmail.com>
2019-07-14 18:29:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def long_planeteer_calls(calls)
   while i < calls.length do |index|
     if index.any? > 4
       return TRUE
+      i+=1
     else
       return FALSE
+      i+=1
     end
     
     i+=1
